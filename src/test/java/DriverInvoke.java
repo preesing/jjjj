0e04e385aaa8804773a288d255bd3e8725184ba9
vijayashreedagam <vijayasri.dagam@valuepitch.com>
2022-08-03 12:36:15 +0530

    file added

---
+++ src/test/java/DriverInvoke.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverInvoke {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		//System.out.println(driver.getTitle());
+		
+	}
+
+}
